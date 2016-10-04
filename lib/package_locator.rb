832ec2b8e656263871e1aa1b98a4cda98515a835
Komaron James <kjames@jibe.com>
2016-10-03 22:17:17 -0400

    Adding a script to run code, make things easier

--- lib/package_locator.rb
+++ lib/package_locator.rb
@@ -1,6 +1,3 @@
-lib = File.expand_path('../../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
 require 'package_locator/core'
 require 'package_locator/fedex'
 require 'package_locator/ups'
