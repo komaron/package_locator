da968560f39d38c980ece7a74c5741248e97c530
Komaron James <kjames@jibe.com>
2016-10-03 21:44:36 -0400

    Building basic FedEx class

--- lib/package_locator/fedex.rb
+++ lib/package_locator/fedex.rb
@@ -1,10 +1,23 @@
-module Carrier
-  class FedEx
+module PackageLocator
+	module Carrier
+	  class FedEx
 
-    def initalize(tracking_number, session)
-      @tracking_number = tracking_number
-      @session = session
-    end
+	    def initialize(tracking_number, session)
+	      @tracking_number = tracking_number
+	      @session = session
+	    end
 
-  end
+	    def self.locate_package(tracking_number,session)
+	      agent = FedEx.new(tracking_number,session)
+	      agent.perform
+	    end
+
+	    def perform
+	      @session.visit 'https://www.fedex.com/apps/fedextrack/'
+	      @session.find(:css, 'textarea[name="trackNumbers"]').set @tracking_number
+	      @session.find_button('Track').click
+	    end
+
+	  end
+	end
 end
