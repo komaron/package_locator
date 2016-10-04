e67d8bcd866a386438e88761469475e6183e4dcd
Komaron James <kjames@jibe.com>
2016-10-03 21:43:17 -0400

    Building UPS class

--- lib/package_locator/ups.rb
+++ lib/package_locator/ups.rb
@@ -1,10 +1,22 @@
-module Carrier
-  class UPS
+module PackageLocator
+  module Carrier
+    class UPS
 
-    def initalize(tracking_number, session)
-      @tracking_number = tracking_number
-      @session = session
+      def initialize(tracking_number, session)
+        @tracking_number = tracking_number
+        @session = session
+      end
+
+      def self.locate_package(tracking_number,session)
+        agent = UPS.new(tracking_number,session)
+        agent.perform
+      end
+
+      def perform
+        @session.visit 'https://www.ups.com/WebTracking/track?loc=en_us'
+        @session.find(:css, 'textarea[id="trackNums"]').set @tracking_number
+        @session.find(:css, 'input[value="Track"]').click
+      end
     end
-
   end
 end
