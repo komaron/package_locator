bdf90aa3efad9441c50fffdd85de71d6176e0dad
Komaron James <kjames@jibe.com>
2016-10-03 21:43:38 -0400

    Building basic usps class

--- lib/package_locator/usps.rb
+++ lib/package_locator/usps.rb
@@ -1,10 +1,22 @@
-module Carrier
-  class USPS
+module PackageLocator
+	module Carrier
+	  class USPS
 
-    def initalize(tracking_number, session)
- 			@tracking_number = tracking_number
- 			@session = session
+	    def initialize(tracking_number, session)
+	 			@tracking_number = tracking_number
+	 			@session = session
+			end
+
+			def self.locate_package(tracking_number,session)
+	      agent = USPS.new(tracking_number,session)
+	      agent.perform
+	    end
+
+	    def perform
+	      @session.visit 'https://tools.usps.com/go/TrackConfirmAction_input'
+	      @session.find(:css, 'textarea[id="tLabels"]').set @tracking_number
+	      @session.find(:css, 'input[value="Find"]').click
+	    end
 		end
-
 	end
 end
