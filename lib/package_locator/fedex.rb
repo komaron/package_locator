9e510340a66c113fa8c381c63a9c93f47cc0b8d9
Komaron James <kjames@jibe.com>
2016-10-03 22:35:44 -0400

    Fixing tab spaces

--- lib/package_locator/fedex.rb
+++ lib/package_locator/fedex.rb
@@ -1,23 +1,23 @@
 module PackageLocator
-	module Carrier
-	  class FedEx
+  module Carrier
+    class FedEx
 
-	    def initialize(tracking_number, session)
-	      @tracking_number = tracking_number
-	      @session = session
-	    end
+      def initialize(tracking_number, session)
+        @tracking_number = tracking_number
+        @session = session
+      end
 
-	    def self.locate_package(tracking_number,session)
-	      agent = FedEx.new(tracking_number,session)
-	      agent.perform
-	    end
+      def self.locate_package(tracking_number,session)
+        agent = FedEx.new(tracking_number,session)
+        agent.perform
+      end
 
-	    def perform
-	      @session.visit 'https://www.fedex.com/apps/fedextrack/'
-	      @session.find(:css, 'textarea[name="trackNumbers"]').set @tracking_number
-	      @session.find_button('Track').click
-	    end
+      def perform
+        @session.visit 'https://www.fedex.com/apps/fedextrack/'
+        @session.find(:css, 'textarea[name="trackNumbers"]').set @tracking_number
+        @session.find_button('Track').click
+      end
 
-	  end
-	end
+    end
+  end
 end
--- lib/package_locator/usps.rb
+++ lib/package_locator/usps.rb
@@ -1,22 +1,22 @@
 module PackageLocator
-	module Carrier
-	  class USPS
+  module Carrier
+    class USPS
 
-	    def initialize(tracking_number, session)
-	 			@tracking_number = tracking_number
-	 			@session = session
-			end
+      def initialize(tracking_number, session)
+        @tracking_number = tracking_number
+        @session = session
+      end
 
-			def self.locate_package(tracking_number,session)
-	      agent = USPS.new(tracking_number,session)
-	      agent.perform
-	    end
+      def self.locate_package(tracking_number,session)
+        agent = USPS.new(tracking_number,session)
+        agent.perform
+      end
 
-	    def perform
-	      @session.visit 'https://tools.usps.com/go/TrackConfirmAction_input'
-	      @session.find(:css, 'textarea[id="tLabels"]').set @tracking_number
-	      @session.find(:css, 'input[value="Find"]').click
-	    end
-		end
-	end
+      def perform
+        @session.visit 'https://tools.usps.com/go/TrackConfirmAction_input'
+        @session.find(:css, 'textarea[id="tLabels"]').set @tracking_number
+        @session.find(:css, 'input[value="Find"]').click
+      end
+    end
+  end
 end
